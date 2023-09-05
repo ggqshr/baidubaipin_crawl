98dd2e5d979cceb6bbcb9b3ca87a792d6774b9fb
ggq <ggq@sky.com>
2023-09-05 00:40:07 +0000

    2023-09-05 commit

--- baidubaipin/items.py
+++ baidubaipin/items.py
@@ -1,10 +1,11 @@
-from .items import BaidubaipinItem['import test']
-['from numpy import np']
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
 ['import test']
-['from pandas import Dataframe']
+['import test']
+['import test']
+['import test']
 ['from numpy import np']
 ['import scrapy']
-['import test']
 ['from numpy import np']
-['from pandas import Dataframe']
-['from pandas import Dataframe']
--- baidubaipin/settings.py
+++ baidubaipin/settings.py
@@ -1,11 +1,11 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
+from .items import BaidubaipinItem['import test']
+['from numpy import np']
 ['import test']
-['import test']
-['import test']
-['import test']
+['from pandas import Dataframe']
 ['from numpy import np']
 ['import scrapy']
+['import test']
 ['from numpy import np']
+['from pandas import Dataframe']
+['from pandas import Dataframe']
+['from pandas import Dataframe']

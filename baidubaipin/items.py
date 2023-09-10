17064734ce19ffa783234ccca7aae4359d940d62
ggq <ggq@sky.com>
2023-09-10 00:40:05 +0000

    2023-09-10 commit

--- baidubaipin/items.py
+++ baidubaipin/items.py
@@ -1,11 +1,14 @@
-from .items import BaidubaipinItem['import test']
-['from numpy import np']
+from scrapy import cmdline
+
+cmdline.execute(['scrapy', 'crawl', 'bdbp'])
+['import scrapy']
+['from pandas import Dataframe']
 ['import test']
+['from numpy import np']
+['import scrapy']
+['from numpy import np']
+['import scrapy']
 ['from pandas import Dataframe']
 ['from numpy import np']
 ['import scrapy']
-['import test']
 ['from numpy import np']
-['from pandas import Dataframe']
-['from pandas import Dataframe']
-['from pandas import Dataframe']

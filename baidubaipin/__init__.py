118490eadf255ab271076c77edbea943d00737ba
ggq <ggq@sky.com>
2023-09-18 00:40:05 +0000

    2023-09-18 commit

--- baidubaipin/__init__.py
+++ baidubaipin/__init__.py
@@ -1,13 +1,17 @@
-from .items import BaidubaipinItem['import test']
+# Automatically created by: scrapyd-deploy
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'project',
+    version      = '1.0',
+    packages     = find_packages(),
+    entry_points = {'scrapy': ['settings = baidubaipin.settings']},
+)
+['import test']
+['import scrapy']
+['from numpy import np']
+['from pandas import Dataframe']
 ['from numpy import np']
 ['import test']
-['from pandas import Dataframe']
-['from numpy import np']
-['import scrapy']
-['import test']
-['from numpy import np']
-['from pandas import Dataframe']
-['from pandas import Dataframe']
-['from pandas import Dataframe']
-['import scrapy']
 ['from pandas import Dataframe']

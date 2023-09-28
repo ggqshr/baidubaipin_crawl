50bac9260f8881ed44432fb259e5c717fd6d364c
ggq <ggq@sky.com>
2023-09-28 00:40:05 +0000

    2023-09-28 commit

--- baidubaipin/spiders/bdbp.py
+++ baidubaipin/spiders/bdbp.py
@@ -1,16 +1,18 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-['import test']
-['import test']
-['import test']
+from scrapy import cmdline
+
+cmdline.execute(['scrapy', 'crawl', 'bdbp'])
+['import scrapy']
+['from pandas import Dataframe']
 ['import test']
 ['from numpy import np']
 ['import scrapy']
 ['from numpy import np']
-['import test']
-['from pandas import Dataframe']
+['import scrapy']
 ['from pandas import Dataframe']
 ['from numpy import np']
 ['import scrapy']
+['from numpy import np']
+['from numpy import np']
+['from numpy import np']
+['from numpy import np']
+['from pandas import Dataframe']

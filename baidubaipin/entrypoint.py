a886ea94c4ff285f336d7ce2c5087e94a3ca23b5
ggq <ggq@sky.com>
2023-11-15 00:40:06 +0000

    2023-11-15 commit

--- baidubaipin/entrypoint.py
+++ baidubaipin/entrypoint.py
@@ -1,35 +1,36 @@
-# Automatically created by: scrapyd-deploy
-
-from setuptools import setup, find_packages
-
-setup(
-    name         = 'project',
-    version      = '1.0',
-    packages     = find_packages(),
-    entry_points = {'scrapy': ['settings = baidubaipin.settings']},
-)
-['import test']
-['import scrapy']
-['from numpy import np']
-['from pandas import Dataframe']
+from .items import BaidubaipinItem['import test']
 ['from numpy import np']
 ['import test']
 ['from pandas import Dataframe']
-['import test']
+['from numpy import np']
 ['import scrapy']
-['from pandas import Dataframe']
 ['import test']
-['import test']
-['import scrapy']
+['from numpy import np']
 ['from pandas import Dataframe']
 ['from pandas import Dataframe']
+['from pandas import Dataframe']
 ['import scrapy']
+['from pandas import Dataframe']
+['import scrapy']
+['from numpy import np']
+['import scrapy']
+['import test']
+['import test']
+['import test']
+['from pandas import Dataframe']
+['from pandas import Dataframe']
+['import scrapy']
+['import scrapy']
+['import test']
+['from pandas import Dataframe']
+['from pandas import Dataframe']
+['from numpy import np']
 ['from numpy import np']
 ['import test']
 ['import scrapy']
 ['import test']
-['import test']
-['from numpy import np']
-['from numpy import np']
-['from numpy import np']
 ['import scrapy']
+['import test']
+['import scrapy']
+['import test']
+['from pandas import Dataframe']

61b9ead7b71b07ab7254825a3ca7ffdcf4128165
ggq <ggq@sky.com>
2023-11-17 00:40:06 +0000

    2023-11-17 commit

--- baidubaipin/pipelines.py
+++ baidubaipin/pipelines.py
@@ -1,35 +1,44 @@
-# Automatically created by: scrapyd-deploy
+from scrapy import cmdline
 
-from setuptools import setup, find_packages
-
-setup(
-    name         = 'project',
-    version      = '1.0',
-    packages     = find_packages(),
-    entry_points = {'scrapy': ['settings = baidubaipin.settings']},
-)
-['import test']
+cmdline.execute(['scrapy', 'crawl', 'bdbp'])
 ['import scrapy']
-['from numpy import np']
-['from pandas import Dataframe']
-['from numpy import np']
-['import test']
 ['from pandas import Dataframe']
 ['import test']
+['from numpy import np']
 ['import scrapy']
+['from numpy import np']
+['import scrapy']
+['from pandas import Dataframe']
+['from numpy import np']
+['import scrapy']
+['from numpy import np']
+['from numpy import np']
+['from numpy import np']
+['from numpy import np']
 ['from pandas import Dataframe']
 ['import test']
 ['import test']
 ['import scrapy']
 ['from pandas import Dataframe']
 ['from pandas import Dataframe']
+['from pandas import Dataframe']
+['import test']
+['from numpy import np']
+['import scrapy']
+['from numpy import np']
+['import scrapy']
+['from numpy import np']
+['from numpy import np']
+['import scrapy']
+['from pandas import Dataframe']
+['from pandas import Dataframe']
+['from numpy import np']
+['import test']
+['from numpy import np']
+['from pandas import Dataframe']
+['from numpy import np']
+['import test']
+['import test']
 ['import scrapy']
 ['from numpy import np']
 ['import test']
-['import scrapy']
-['import test']
-['import test']
-['from numpy import np']
-['from numpy import np']
-['from numpy import np']
-['import scrapy']
